5146d8398b5b75193063093195307a3032c9ac04
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-05-31 23:12:32 -0700

    DONE

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
+console.log('hey you')
+
+server.listen(port,()=>{
+    console.log('server listening', port)
+})
